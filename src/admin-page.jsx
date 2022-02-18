51c5096e4723d6f79cee3b1eb6b514c41fb6407a
Matt Dellandrea <mdellandrea@atlassian.com>
2022-02-18 14:25:02 -0600

    COMPASS-5650 updating examples and correcting formatting

--- src/admin-page.jsx
+++ src/admin-page.jsx
@@ -1,11 +1,15 @@
-import ForgeUI, { render, Fragment, Text } from '@forge/ui';
+import ForgeUI, {
+  render,
+  Fragment,
+  Text,
+} from '@forge/ui';
 
 const App = () => {
-  return(
-      <Fragment>
-        <Text>Hello world - admin page</Text>
-      </Fragment>
-    )
+  return (
+    <Fragment>
+      <Text>Hello world - admin page</Text>
+    </Fragment>
+  );
 };
 
 export const run = render(
